6eb0283f3d64b9a821c4694f8464c01be8620f0a
Carl Wang <oryjk@qq.com>
2016-06-22 09:51:01 +0800

    initial

---
+++ src/main/java/com/carlwang/ConfigServiceApplication.java
@@ -0,0 +1,17 @@
+package com.carlwang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by carlwang on 6/21/16.
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServiceApplication.class, args);
+    }
+}
